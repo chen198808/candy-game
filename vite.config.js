e476be600bcd8961cc2cbc4552c613c0e98a862e
chen198808 <151659872+chen198808@users.noreply.github.com>
2026-07-11 22:45:36 +0800

    Disable server settings in vite.config.js
    Comment out server configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    port: 3000,
-    open: true
+  // server: {
+//   port: 3000,
+//   open: true
+// }
+
   }
 })
  
